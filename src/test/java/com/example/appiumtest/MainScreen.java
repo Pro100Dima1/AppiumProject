61c3dcad4617a95c19cd97bec2cdea8cfbc9d564
Pro100Dima1 <dima.sinkin@yandex.ru>
2025-01-05 15:58:44 +0300

    Add 10 Appium test Calendar

--- src/test/java/com/example/appiumtest/MainScreen.java
+++ src/test/java/com/example/appiumtest/MainScreen.java
@@ -10,10 +10,79 @@ public class MainScreen {
 
     @AndroidFindBy(xpath = "\t\n" +
             "//android.widget.ImageButton[@content-desc=\"Следующий месяц\"]")
-    private WebElement header;
+    private WebElement nextMonth;
 
-    public boolean checkHeader() {
-        return header.isEnabled();
+    @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"Прошлый месяц\"]")
+    private WebElement pastMonth;
+
+    @AndroidFindBy(xpath = "//android.widget.LinearLayout[@resource-id=\"android:id/date_picker_header\"]/android.widget.LinearLayout\n")
+    private WebElement headWidget;
+
+    @AndroidFindBy(xpath = "//android.view.View[@resource-id=\"android:id/month_view\"]")
+    private WebElement layoutCalendar;
+
+    @AndroidFindBy(xpath = "//android.widget.Button[@resource-id=\"com.calendar.clever.cod:id/button2\"]")
+    private WebElement headData;
+
+    @AndroidFindBy(xpath = "//android.widget.ImageButton[@content-desc=\"Календарь\"]")
+    private WebElement toolBar;
+
+    @AndroidFindBy(xpath = "//android.widget.ImageView[@resource-id=\"com.calendar.clever.cod:id/imageview7\"]")
+    private WebElement banner;
+
+    @AndroidFindBy(xpath = "\t\n" +
+            "//android.widget.TextView[@resource-id=\"com.calendar.clever.cod:id/textview3\"]")
+    private WebElement rateTheApp;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id=\"android:id/alertTitle\"]")
+    private WebElement estimation;
+
+    @AndroidFindBy(xpath = "//android.widget.TextView[@resource-id=\"com.calendar.clever.cod:id/textview1\"]")
+    private WebElement toolBarText;
+
+    public boolean checkNextMonth() {
+        return nextMonth.isEnabled();
+    }
+
+    public boolean checkPastMonth() {
+        return pastMonth.isEnabled();
+    }
+
+    public boolean checkHeadWidget() {
+        return headWidget.isEnabled();
+    }
+
+    public boolean checkLayoutCalendar() {
+        return layoutCalendar.isEnabled();
+    }
+
+    public boolean checkHeadData() {
+        return headData.isEnabled();
+    }
+
+    public boolean checkToolBar() {
+        return toolBar.isEnabled();
+    }
+
+    public boolean checkToolBarIsClickable() {
+        toolBar.click();
+        return banner.isEnabled();
+    }
+
+    public boolean checkRateTheApp() {
+        toolBar.click();
+        return rateTheApp.isEnabled();
+    }
+
+    public boolean checkRateTheAppIsClickable() {
+        toolBar.click();
+        rateTheApp.click();
+        return estimation.isEnabled();
+    }
+
+    public String checkToolBarText() {
+        toolBar.click();
+        return toolBarText.getText();
     }
 
     public MainScreen(AndroidDriver driver) {
--- src/test/java/com/example/appiumtest/MainScreenTest.java
+++ src/test/java/com/example/appiumtest/MainScreenTest.java
@@ -1,51 +1,99 @@
 package com.example.appiumtest;
-import io.appium.java_client.remote.options.BaseOptions;
+
 import io.appium.java_client.android.AndroidDriver;
-
-import java.net.MalformedURLException;
-import java.net.URL;
-
+import io.appium.java_client.remote.options.BaseOptions;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainScreenTest {
 
-        private AndroidDriver driver;
-        private URL serverUrl;
-        private MainScreen MainScreen;
+    private AndroidDriver driver;
+    private URL serverUrl;
+    private MainScreen MainScreen;
 
-        @Before
-        public void setUp() throws MalformedURLException {
-            BaseOptions options = new BaseOptions()
-                    .amend("appium:app", "G:\\apk\\Простой Календарь_4.1.9_APKPure.apk")
-                    .amend("platformName", "Android")
-                    .amend("appium:deviceName", "SamsungS20FE")
-                    .amend("appium:automationName", "uiAutomator2")
-                    .amend("appium:ensureWebviewsHavePages", true)
-                    .amend("appium:nativeWebScreenshot", true)
-                    .amend("appium:newCommandTimeout", 3600)
-                    .amend("appium:connectHardwareKeyboard", true);
+    @Before
+    public void setUp() throws MalformedURLException {
+        BaseOptions options = new BaseOptions()
+                .amend("appium:app", "G:\\apk\\Простой Календарь_4.1.9_APKPure.apk")
+                .amend("platformName", "Android")
+                .amend("appium:deviceName", "SamsungS20FE")
+                .amend("appium:automationName", "uiAutomator2")
+                .amend("appium:ensureWebviewsHavePages", true)
+                .amend("appium:nativeWebScreenshot", true)
+                .amend("appium:newCommandTimeout", 3600)
+                .amend("appium:connectHardwareKeyboard", true);
 
-            // Создаем URL сервера Appium
-            serverUrl = new URL("http://127.0.0.1:4723");
+        // Создаем URL сервера Appium
+        serverUrl = new URL("http://127.0.0.1:4723");
 
-            // Инициализация драйвера
-            driver = new AndroidDriver(serverUrl, options);
+        // Инициализация драйвера
+        driver = new AndroidDriver(serverUrl, options);
 
-            MainScreen = new MainScreen(driver);
-        }
+        MainScreen = new MainScreen(driver);
+    }
 
-        @Test
-        public void sampleTest() {
-            assertTrue(MainScreen.checkHeader(), "Нет хедера");
-        }
+    @Test
+    public void checkNextMonthIsEnabled() {
+        assertTrue(MainScreen.checkNextMonth(), "Кнопка перехода на следующий месяц доступна");
+    }
 
-        @After
-        public void tearDown() {
-            driver.quit();
-        }
+    @Test
+    public void checkPastMonthIsEnabled() {
+        assertTrue(MainScreen.checkPastMonth(), "Кнопка перехода на прошлый месяц доступна");
+    }
+
+    @Test
+    public void checkHeadWidgetIsEnabled() {
+        assertTrue(MainScreen.checkHeadWidget(), "Виджет на главной странице есть");
+    }
+
+    @Test
+    public void checkLayoutCalendarIsEnabled() {
+        assertTrue(MainScreen.checkLayoutCalendar(), "Календарь есть на главной странице");
+    }
+
+    @Test
+    public void checkHeadDataIsEnabled() {
+        assertTrue(MainScreen.checkHeadData(), "Дата есть в шапке главной страницы");
+    }
+
+    @Test
+    public void checkToolBarIsEnabled() {
+        assertTrue(MainScreen.checkToolBar(), "На главной странице доступен тулбар");
+    }
+
+    @Test
+    public void checkToolBarIsClickable() {
+        assertTrue(MainScreen.checkToolBarIsClickable(), "На главной странице тулбар при нажатии открывает менюшку");
+    }
+
+    @Test
+    public void checkRateTheApp() {
+        assertTrue(MainScreen.checkRateTheApp(), "В менюшке тулбара есть кнопка Оценить приложение");
+    }
+
+    @Test
+    public void checkRateTheAppIsClickable() {
+        assertTrue(MainScreen.checkRateTheAppIsClickable(), "В меню тулбара кнопка Оценить приложение кликабельна");
+    }
+
+    @Test
+    public void checkToolBarTextIsCorrect() {
+        String expectedText = "Календарь Clever";
+        String actualText = MainScreen.checkToolBarText();
+        assertEquals(expectedText, actualText, "Текст не совпадают");
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
 
 }
